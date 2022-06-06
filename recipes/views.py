ab2926538ac5f3432601f1e84ec22d8dfb79fff2
thiagodev <thiagodev@thiago>
2022-06-06 03:13:14 -0300

    staticfiles

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,10 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
 
+context = {
+    'name': 'Easy',
+}
+
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html', context=context)
