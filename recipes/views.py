473f27d67b0e5f713affbe0a912d31ce9682b522
thiagodev <thiagodev@thiago>
2022-06-08 23:18:27 -0300

    templates herança

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,24 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
 
-context = {
-    'name': 'Easy',
+home_context = {
+    'name': 'Easy Recipes',
+    'title': 'Home',
+}
+
+recipe_context = {
+    'name': 'Easy Recipes',
+    'title': 'Receita',
 }
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context=context)
+    return render(request,
+                  'recipes/pages/home.html',
+                  context=home_context)
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html', context=context)
+    return render(request,
+                  'recipes/pages/recipe-view.html',
+                  context=recipe_context)
